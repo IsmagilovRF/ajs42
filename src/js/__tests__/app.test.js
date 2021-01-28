0a43405c848586833bcbeeeea6355ef53a14f05b
IsmagilovRF <ledohod@icloud.com>
2021-01-28 13:19:44 +0300

    sort not.toBe

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,6 +1,6 @@
 import sortByHealth from '../app';
 
-test('sort', () => {
+test('sort toEqual', () => {
   const input = [
     { name: 'мечник', health: 10 },
     { name: 'маг', health: 100 },
@@ -14,4 +14,22 @@ test('sort', () => {
   ];
 
   expect(sortByHealth(input)).toEqual(expected);
+
+});
+
+test('sort not.toBe', () => {
+  const input = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+
+  const expected = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+
+  expect(sortByHealth(input)).not.toBe(expected);
+
 });
